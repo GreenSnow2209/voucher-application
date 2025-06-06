e5fd6458bd3563693084fdd5e7edbbb6e4a8f815
Son Tran <son.tranthanh@hdwebsoft.dev>
2025-06-06 18:04:01 +0700

    Draft

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './swagger';
+import {swaggerSpec} from './swagger';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import routes from './routes';
@@ -11,22 +11,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    const mongoUri = process.env.MONGO_URI || "";
-    await mongoose.connect(mongoUri);
-    console.log("Connected to mongodb successfully!");
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    try {
+        const mongoUri = process.env.MONGO_URI || "";
+        await mongoose.connect(mongoUri);
+        console.log("Connected to mongodb successfully!");
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
 };
 
 start().then(() => {
-  app.use(express.json());
-  app.use(cors());
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.use('/api', routes);
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  })
+    app.use(express.json());
+    app.use(cors());
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api', routes);
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    })
 });
