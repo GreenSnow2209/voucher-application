111e835bc29c38b8a512023b06e5abb84b4ca9f3
Son Tran <son.tranthanh@hdwebsoft.dev>
2025-07-02 10:40:01 +0700

    Update unit test

--- src/tests/setup/mock-mongo.ts
+++ src/tests/setup/mock-mongo.ts
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
+import { appConfig } from '../../config/app.config';
 
 let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  console.log('Mongo URI:', uri);
-  await mongoose.connect(uri);
+  const mongoUri = appConfig.mongoURI || '';
+  const databaseName = appConfig.databaseName || 'voucher_app';
+  await mongoose.connect(mongoUri + '/' + databaseName);
+  //const uri = mongoServer.getUri();
+  //console.log('Mongo URI:', uri);
+  await mongoose.connect(mongoUri + '/' + databaseName);
 }, 30_000);
 
 afterAll(async () => {
